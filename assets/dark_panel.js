c4b089215de1c8a23338500fdfe96c6bc2ec2088
Johnny Tsai <johnny.tsai@iot-hmi.com>
2021-01-04 07:02:46 +0800

    save for sync

--- assets/dark_panel.js
+++ assets/dark_panel.js
@@ -1,48 +1,59 @@
-! function (e) {
-    var t = {};
+! function (modules) {
+    var installedModules = {};
 
-    function n(o) {
-        if (t[o]) return t[o].exports;
-        var r = t[o] = {
-            i: o,
-            l: !1,
+    function __webpack_require__(moduleId) {
+        if (installedModules[moduleId]) return installedModules[moduleId].exports;
+        var module = installedModules[moduleId] = {
+            i: moduleId,
+            l: false,
             exports: {}
         };
-        return e[o].call(r.exports, r, r.exports, n), r.l = !0, r.exports
+        modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+        module.l = true;
+        return module.exports
     }
-    n.m = e, n.c = t, n.d = function (e, t, o) {
-        n.o(e, t) || Object.defineProperty(e, t, {
-            enumerable: !0,
-            get: o
-        })
-    }, n.r = function (e) {
+    __webpack_require__.m = modules;
+    __webpack_require__.c = installedModules;
+    __webpack_require__.d = function (exports, name, getter) {
+        if(!__webpack_require__.o(exports, name)) {
+            Object.defineProperty(exports, name, {
+                enumerable: true,
+                get: getter
+            });
+        }
+    };
+    __webpack_require__.r = function (e) {
         "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
             value: "Module"
         }), Object.defineProperty(e, "__esModule", {
-            value: !0
+            value: true
         })
-    }, n.t = function (e, t) {
-        if (1 & t && (e = n(e)), 8 & t) return e;
+    };
+    __webpack_require__.t = function (e, t) {
+        if (1 & t && (e = __webpack_require__(e)), 8 & t) return e;
         if (4 & t && "object" == typeof e && e && e.__esModule) return e;
         var o = Object.create(null);
-        if (n.r(o), Object.defineProperty(o, "default", {
-                enumerable: !0,
+        if (__webpack_require__.r(o), Object.defineProperty(o, "default", {
+                enumerable: true,
                 value: e
             }), 2 & t && "string" != typeof e)
-            for (var r in e) n.d(o, r, function (t) {
+            for (var r in e) __webpack_require__.d(o, r, function (t) {
                 return e[t]
             }.bind(null, r));
         return o
-    }, n.n = function (e) {
+    }; 
+    __webpack_require__.n = function (e) {
         var t = e && e.__esModule ? function () {
             return e.default
         } : function () {
             return e
         };
-        return n.d(t, "a", t), t
-    }, n.o = function (e, t) {
+        return __webpack_require__.d(t, "a", t), t
+    };
+    __webpack_require__.o = function (e, t) {
         return Object.prototype.hasOwnProperty.call(e, t)
-    }, n.p = "", n(n.s = 293)
+    };
+    __webpack_require__.p = "", __webpack_require__(__webpack_require__.s = 293)
 }({
     293: function (e, t, n) {
         e.exports = n(294)
@@ -74,24 +85,45 @@
                                     a = document.querySelector(".tablebox").offsetTop,
                                     i = document.querySelector(".featurebox").offsetTop,
                                     c = .8 * window.innerHeight;
-                                e > t - c ? $(".linebox").addClass("active") : $(".linebox").removeClass("active"), e > n - c ? $(".upline").addClass("active") : $(".upline").removeClass("active"), e > o - c ? $(".downline").addClass("active") : $(".downline").removeClass("active"), e > r - c ? $(".descbox").addClass("active") : $(".descbox").removeClass("active"), e > a - c ? $(".tablebox").addClass("active") : $(".tablebox").removeClass("active"), e > i - c ? $(".featurebox").addClass("active") : $(".featurebox").removeClass("active")
+                                e > t - c ? $(".linebox").addClass("active") : $(".linebox").removeClass("active"), 
+                                e > n - c ? $(".upline").addClass("active") : $(".upline").removeClass("active"), 
+                                e > o - c ? $(".downline").addClass("active") : $(".downline").removeClass("active"), 
+                                e > r - c ? $(".descbox").addClass("active") : $(".descbox").removeClass("active"), 
+                                e > a - c ? $(".tablebox").addClass("active") : $(".tablebox").removeClass("active"), 
+                                e > i - c ? $(".featurebox").addClass("active") : $(".featurebox").removeClass("active")
                             }
                         }
                     }), $("#crtlRowBtnFrame").on("click", ".touchBox", function () {
                         a($(this).index())
                     }), setTimeout(function () {
                         r()
-                    }, 5e3), $(".carousel .banner").eq($(".banner").length).addClass("nowleave"), $(".carousel .banner").eq(0).addClass("active"), $("#crtlRowBtnFrame .touchBox").eq(0).addClass("nowSliders")
+                    }, 5e3), 
+                    $(".carousel .banner").eq($(".banner").length).addClass("nowleave"), 
+                    $(".carousel .banner").eq(0).addClass("active"), 
+                    $("#crtlRowBtnFrame .touchBox").eq(0).addClass("nowSliders")
                 });
                 var t = 0,
                     o = $(".banner").length,
                     r = function () {
-                        $(".carousel .banner").removeClass("nowleave"), $(".carousel .banner").removeClass("active"), $("#crtlRowBtnFrame .touchBox").removeClass("nowSliders"), t + 1 === $(".carousel .banner").length ? (t = 0, o = $(".carousel .banner").length) : (o = t, t += 1), $(".carousel .banner").eq(o).addClass("nowleave"), $(".carousel .banner").eq(t).addClass("active"), $("#crtlRowBtnFrame .touchBox").eq(t).addClass("nowSliders"), setTimeout(function () {
+                        $(".carousel .banner").removeClass("nowleave"), 
+                        $(".carousel .banner").removeClass("active"), 
+                        $("#crtlRowBtnFrame .touchBox").removeClass("nowSliders"), 
+                        t + 1 === $(".carousel .banner").length ? (t = 0, o = $(".carousel .banner").length) : (o = t, t += 1), 
+                        $(".carousel .banner").eq(o).addClass("nowleave"), 
+                        $(".carousel .banner").eq(t).addClass("active"), 
+                        $("#crtlRowBtnFrame .touchBox").eq(t).addClass("nowSliders"), 
+                        setTimeout(function () {
                             r()
                         }, 5e3)
                     },
                     a = function (e) {
-                        $(".carousel .banner").removeClass("nowleave"), $(".carousel .banner").removeClass("active"), $("#crtlRowBtnFrame .touchBox").removeClass("nowSliders"), t === e ? (o = o, t = e) : (o = t, t = e), $(".carousel .banner").eq(o).addClass("nowleave"), $(".carousel .banner").eq(t).addClass("active"), $("#crtlRowBtnFrame .touchBox").eq(t).addClass("nowSliders")
+                        $(".carousel .banner").removeClass("nowleave"), 
+                        $(".carousel .banner").removeClass("active"), 
+                        $("#crtlRowBtnFrame .touchBox").removeClass("nowSliders"), 
+                        t === e ? (o = o, t = e) : (o = t, t = e), 
+                        $(".carousel .banner").eq(o).addClass("nowleave"), 
+                        $(".carousel .banner").eq(t).addClass("active"), 
+                        $("#crtlRowBtnFrame .touchBox").eq(t).addClass("nowSliders")
                     }
             }.call(this, n(5))
     },
@@ -132,22 +164,22 @@
             }
         }();
         var u, l = [],
-            s = !1,
+            s = false,
             d = -1;
 
         function f() {
-            s && u && (s = !1, u.length ? l = u.concat(l) : d = -1, l.length && m())
+            s && u && (s = false, u.length ? l = u.concat(l) : d = -1, l.length && m())
         }
 
         function m() {
             if (!s) {
                 var e = c(f);
-                s = !0;
+                s = true;
                 for (var t = l.length; t;) {
                     for (u = l, l = []; ++d < t;) u && u[d].run();
                     d = -1, t = l.length
                 }
-                u = null, s = !1,
+                u = null, s = false,
                     function (e) {
                         if (o === clearTimeout) return clearTimeout(e);
                         if ((o === i || !o) && clearTimeout) return o = clearTimeout, clearTimeout(e);
@@ -173,18 +205,39 @@
             var t = new Array(arguments.length - 1);
             if (arguments.length > 1)
                 for (var n = 1; n < arguments.length; n++) t[n - 1] = arguments[n];
-            l.push(new v(e, t)), 1 !== l.length || s || c(m)
-        }, v.prototype.run = function () {
+            l.push(new v(e, t)), 
+            1 !== l.length || s || c(m)
+        }, 
+        v.prototype.run = function () {
             this.fun.apply(null, this.array)
-        }, r.title = "browser", r.browser = !0, r.env = {}, r.argv = [], r.version = "", r.versions = {}, r.on = b, r.addListener = b, r.once = b, r.off = b, r.removeListener = b, r.removeAllListeners = b, r.emit = b, r.prependListener = b, r.prependOnceListener = b, r.listeners = function (e) {
+        }, 
+        r.title = "browser";
+        r.browser = true; 
+        r.env = {}, r.argv = [];
+        r.version = "";
+        r.versions = {};
+        r.on = b;
+        r.addListener = b, 
+        r.once = b, 
+        r.off = b, 
+        r.removeListener = b, 
+        r.removeAllListeners = b, 
+        r.emit = b, 
+        r.prependListener = b, 
+        r.prependOnceListener = b, 
+        r.listeners = function (e) {
             return []
-        }, r.binding = function (e) {
+        }, 
+        r.binding = function (e) {
             throw new Error("process.binding is not supported")
-        }, r.cwd = function () {
+        }, 
+        r.cwd = function () {
             return "/"
-        }, r.chdir = function (e) {
+        }, 
+        r.chdir = function (e) {
             throw new Error("process.chdir is not supported")
-        }, r.umask = function () {
+        }, 
+        r.umask = function () {
             return 0
         }
     }
