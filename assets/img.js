2231dea915d4c9e98c7b15536e003fc00247a47f
Johnny Tsai <johnny.tsai@iot-hmi.com>
2021-02-25 13:41:14 +0800

    2021/02/25 001

--- assets/img.js
+++ assets/img.js
@@ -1,15 +1,52 @@
-$(document).ready(function() {
-    $(".scroll_horizontal").on("wheel", function(e) {
-        if (e.originalEvent.deltaY > 0) {
-            //console.log("scrollLeft:", e);
-            e.currentTarget.scrollLeft += 100;
-        } else {
-            //console.log("scrollRight:", e);
-            e.currentTarget.scrollLeft -= 100;
+$(document).ready(function () {
+    $(".scroll_horizontal").on("wheel", function (e) {
+        if (e.currentTarget.scrollWidth > e.currentTarget.clientWidth) {
+            if (e.originalEvent.deltaY > 0) {
+                e.currentTarget.scrollLeft += 100;
+            } else {
+                e.currentTarget.scrollLeft -= 100;
+            }
+            e.preventDefault();
         }
-        e.preventDefault();
     });
-    $("img.iotimg-background").on( "error", function () {
+    $(".scroll_horizontal_next").on("click", function () {
+        container = $(this).closest(".scroll_horizontal_container");
+        target = container.find(".scroll_horizontal");
+        target.scrollLeft(target.scrollLeft() + 100);
+    });
+    $(".scroll_horizontal_prev").on("click", function () {
+        container = $(this).closest(".scroll_horizontal_container");
+        target = container.find(".scroll_horizontal");
+        target.scrollLeft(target.scrollLeft() - 100);
+    });
+    $(".scroll_horizontal_container").hover(
+        function () {
+            // enter
+            horizontal = $(this).find(".scroll_horizontal");
+            if (horizontal) {
+                horizontal.addClass("scroll_horizontal_cursor");
+            }
+            if (horizontal[0].scrollWidth > horizontal[0].clientWidth) {
+                $(this).addClass("scroll_horizontal_cursor");
+                prev = $(this).find(".scroll_horizontal_prev");
+                next = $(this).find(".scroll_horizontal_next");
+                prev.show();
+                next.show();
+            }
+        },
+        function () {
+            // leave
+            horizontal = $(this).find(".scroll_horizontal");
+            if (horizontal) {
+                horizontal.removeClass("scroll_horizontal_cursor");
+            }
+            container = $(this).closest(".scroll_horizontal_container");
+            prev = $(this).find(".scroll_horizontal_prev");
+            next = $(this).find(".scroll_horizontal_next");
+            prev.hide();
+            next.hide();
+        });
+    $("img.iotimg-background").on("error", function () {
         console.log(this);
         w1 = $(this).closest(".iotimg").width();
         h1 = $(this).closest(".iotimg").height();
@@ -17,7 +54,7 @@ $(document).ready(function() {
         $(this).height("100vh");
         w2 = $(this).closest(".iotimg").width();
         h2 = $(this).closest(".iotimg").height();
-        $(this).width(w1>w2?w1:w2);
-        $(this).height(h1>h2?h1:h2);
+        $(this).width(w1 > w2 ? w1 : w2);
+        $(this).height(h1 > h2 ? h1 : h2);
     });
 });
--- assets/sampledetail.js
+++ assets/sampledetail.js
@@ -1,36 +1,9 @@
 $(document).ready(function() {
-    $(".qlink_cat").hide();
-    $(".qlink_item").hide();
     $(".thumbnail > img").click(function () {
         $(".primarypic > img").width("auto").height("auto").attr("src",$(this).attr("src"));
     });   
-    /*
-    $(".quicklinkarea").click(function () {
-        $(this).toggleClass("quicklinkarea_expand");
-        if ( $(this).hasClass("quicklinkarea_expand") ) {
-            $(this).css({
-                "position": "absolute", 
-                "top": $(this).attr("top"),
-                "left": $(this).attr("left"),
-                "width": $(".rest").width(),
-            });
-            $(".qlink_label").hide();
-            $(".qlink_cat").show();
-            $(".qlink_item").show();
-            $(".quicklinkarea_expand").blur( function() {
-                $(".quicklinkarea").removeClass("quicklinkarea_expand");
-                $(".qlink_label").show();
-                $(".qlink_cat").hide();
-                $(".qlink_item").hide();  
-                $(".quicklinkarea").css("position","");  
-            });
-        } else {
-            $(".qlink_label").show();
-            $(".qlink_cat").hide();
-            $(".qlink_item").hide();
-            $(".quicklinkarea").css("position","");
-        }
-    });*/
+    $(".qlink_cat").hide();
+    $(".qlink_item").hide();
     $(".quicklinkarea").hover(
         function (){
             $(this).addClass("quicklinkarea_expand");
